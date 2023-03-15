5c54a296faee95510f0c6e5393db8353ce609fb6
taponbiswas007 <taponbiswassdp@gmail.com>
2023-03-15 12:52:31 +0600

    responsive slide change

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3.5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+var swiper = new Swiper(".responmySwiper", {
   slidesPerView: 2.5,
   spaceBetween: 30,
   pagination: {
@@ -6,17 +14,14 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+var swiper = new Swiper(".responmySwiper515", {
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
-// Detect screen size and update swiper options
-var mediaQuery = window.matchMedia('(max-width: 600px)');
-function handleMediaQuery(mediaQuery) {
-  if (mediaQuery.matches) {
-    swiper.params.slidesPerView = 1.5;
-    swiper.update();
-  } else {
-    swiper.params.slidesPerView = 3.5;
-    swiper.update();
-  }
-}
-handleMediaQuery(mediaQuery); // Call once on page load
-mediaQuery.addListener(handleMediaQuery); // Call on screen size change
+
+
